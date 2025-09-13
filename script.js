523e71f4fa3adb91fc94f66e53d7e1abaf13e5b8
pda22012 <pda22012@go.uop.gr>
2025-09-13 13:48:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  // Hide the start screen
+  const startScreen = document.getElementById('startScreen');
+  startScreen.style.display = 'none';
+
+  // Show the A-Frame scene
+  const scene = document.getElementById('scene');
+  scene.setAttribute('visible', true);
+
+  console.log("Game started!");
+}
